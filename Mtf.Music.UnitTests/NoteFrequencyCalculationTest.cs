0b17f5ae66b7ac81f3442f786ba6e6a39efac349
Mortens <mortens.4444@gmail.com>
2026-05-30 01:09:48 +0200

    Add unit test

---
+++ Mtf.Music.UnitTests/NoteFrequencyCalculationTest.cs
@@ -0,0 +1,21 @@
+using Mtf.Music.Notes;
+
+namespace Mtf.Music.UnitTests;
+
+public class NoteFrequencyCalculationTests
+{
+    private static readonly Dictionary<Note, double> expectations = new()
+    {
+        { new A4(), 440 },
+        { new C4(), 261.62556530059862 }
+    };
+
+    [Test]
+    public void TestNoteFrequencies()
+    {
+        foreach (var expectation in expectations)
+        {
+            Assert.That(expectation.Key.Frequency, Is.EqualTo(expectation.Value));
+        }
+    }
+}
